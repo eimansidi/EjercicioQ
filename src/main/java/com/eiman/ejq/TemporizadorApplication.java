5a7466c0f5f01a2a65004a3843556683fcc03cfc
Eiman Salek <eimanabde04@gmail.com>
2024-11-14 17:54:54 +0100

    En funcionamiento

--- src/main/java/com/eiman/ejq/TemporizadorApplication.java
+++ src/main/java/com/eiman/ejq/TemporizadorApplication.java
@@ -3,21 +3,26 @@ package com.eiman.ejq;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class TemporizadorApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TemporizadorApplication.class.getResource("EjQ.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        // Cargar el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("EjQ.fxml"));
+        GridPane root = loader.load();
+
+        // Establecer la escena
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("TEMPORIZADOR");
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
